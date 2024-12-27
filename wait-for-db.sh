ba99e52a497c2f15165bdae66f2f9403706c9e6c
Jan Gustafsson <jannegpriv@gmail.com>
2024-12-27 23:02:07 +0100

    Fix database URL parsing in wait-for-db.sh script

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -3,8 +3,14 @@
 
 set -e
 
+# DATABASE_URL format: postgresql://postgres:postgres@postgres/taskmanagement
+host=$(echo $DATABASE_URL | cut -d@ -f2 | cut -d/ -f1)
+user=$(echo $DATABASE_URL | cut -d/ -f3 | cut -d: -f1)
+password=$(echo $DATABASE_URL | cut -d: -f3 | cut -d@ -f1)
+db=$(echo $DATABASE_URL | cut -d/ -f4)
+
 echo "Waiting for PostgreSQL to be ready..."
-until PGPASSWORD=$DB_PASSWORD psql -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -c '\q'; do
+until PGPASSWORD=$password psql -h "$host" -U "$user" -d "$db" -c '\q'; do
   >&2 echo "PostgreSQL is unavailable - sleeping"
   sleep 1
 done
