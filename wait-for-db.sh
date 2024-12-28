bee8b9e16d2b5ae1bcdc37a1f3005adfa993b587
Jan Gustafsson <jannegpriv@gmail.com>
2024-12-28 19:14:02 +0100

    refactor: simplify wait-for-db.sh to use DATABASE_URL directly

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 # wait-for-db.sh
-
 set -e
 
-# DATABASE_URL format: postgresql://postgres:postgres@postgres/taskmanagement
-host=$(echo $DATABASE_URL | cut -d@ -f2 | cut -d/ -f1)
-user=$(echo $DATABASE_URL | cut -d/ -f3 | cut -d: -f1)
-password=$(echo $DATABASE_URL | cut -d: -f3 | cut -d@ -f1)
-db=$(echo $DATABASE_URL | cut -d/ -f4)
-
 echo "Waiting for PostgreSQL to be ready..."
-until PGPASSWORD=$password psql -h "$host" -U "$user" -d "$db" -c '\q'; do
+until psql "$DATABASE_URL" -c '\q' 2>/dev/null; do
   >&2 echo "PostgreSQL is unavailable - sleeping"
   sleep 1
 done
